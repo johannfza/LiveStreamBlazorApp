c5230f13575aa0759de4da9f4af9cc7bf1a80261
juggernauntjo <johannfza@gmail.com>
2020-04-27 15:39:33 +0800

    From MacOs

---
+++ MediaModels/LiveStream.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MediaModels
+{
+    public class LiveStream
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Url { get; set; }
+        public string DatePublished { get; set; }
+        public int Views { get; set; }
+
+        public LiveStream()
+        {
+        }
+
+        public LiveStream(int id, string title, string description, string url)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Url = url;
+            DatePublished = DateTime.UtcNow.ToLocalTime().ToString();
+            Views = 0;
+
+        }
+
+        public override string ToString()
+        {
+            return Title + ": " + Description;
+        }
+    }
+
+}
---
+++ MediaModels/ServerApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MediaModels
+{
+    public class ServerApplication
+    {
+
+        public string Address { get; set; }
+        public string Port { get; set; }
+        public string AppName { get; set; }
+
+        public ServerApplication(string address, string port, string appName)
+        {
+            Address = address;
+            Port = port;
+            AppName = appName;
+        }
+
+        public ServerApplication()
+        {
+        }
+    }
+
+
+}
